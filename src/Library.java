5d2af07d6285c58162b9f3dd2be5cb7048ba29cc
Zheng Shenglong <longdui_1012@qq.com>
2026-09-14 15:04:06 +0800

    图书管理系统-第3步：HashMap按编号查找

--- src/Library.java
+++ src/Library.java
@@ -1,24 +1,29 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 class Library
 {
-    private ArrayList<Book> books;
+    private HashMap <String,Book> books;
 
     Library()
     {
-        books = new ArrayList<>();
+        books = new HashMap<>();
     }
 
     void addBook(Book b)
     {
-        books.add(b);
+        books.put(b.getId(),b);
+
     }
 
     void showAll()
     {
-        for (Book b : books)
+        for (Book b : books.values())
         {
             b.show();
         }
+    }
+    Book findById(String id)
+    {
+        return books.get(id);      // 报编号，直接拿书；没有就返回 null
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main
         lib.addBook(new Book("B002", "三体", "刘慈欣", true));
 
         lib.showAll();
+
+        // 查存在的书
+        Book b = lib.findById("B001");
+        b.show();
+
+        // 查不存在的书
+        System.out.println(lib.findById("B999"));
     }
 }
